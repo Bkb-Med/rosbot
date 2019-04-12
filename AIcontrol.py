103baa29a288f3ab7cbbbaac49d341eee6a4066b
Bkb-Med <49573200+Bkb-Med@users.noreply.github.com>
2019-04-13 00:00:31 +0200

    Add files via upload

---
+++ AIcontrol.py
@@ -0,0 +1,105 @@
+#!/usr/bin/env python 
+import numpy as np
+import rospy
+import cv2
+import os
+import time
+
+from sensor_msgs.msg import CompressedImage
+#from sim_ws.msg import velocity
+from publish_tuto.msg import velo
+
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+from tensorflow import keras 
+from keras.models import load_model
+from keras.utils import CustomObjectScope
+from keras.initializers import glorot_uniform
+from keras import backend as K
+import tensorflow as tf
+with CustomObjectScope({'GlorotUniform': glorot_uniform()}):
+    model = load_model('model/model030419.model')
+graph = tf.get_default_graph()
+
+
+class recognizing_img(object):
+    def __init__(self):
+        self.class_names=["right","left","forward","backward"]
+        self.state="initializing..."
+        self.font=cv2.FONT_HERSHEY_SIMPLEX
+        self.t1 = time.clock()
+        #ros
+        rospy.init_node('velocity_command')
+        self.pub=rospy.Publisher('/topic_value',velo,queue_size=1)
+	self.msgValue=velo()   
+    def img_bridge(self,msg):
+        try:
+            np_arr = np.fromstring(msg.data, np.uint8)
+	    image_np = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+            # convert sensor_msgs/Image to OpenCV Image
+            orig = cv2.flip(image_np,-1)
+            
+            
+            #print "orig"+str(orig.shape)
+            cv2.putText(orig,str(self.state),(20, 40), self.font, 1,(0,255,0),1,cv2.LINE_AA)
+            cv2.putText(orig,"time:%.3f"%(time.clock()-self.t1),(20, 80), self.font, 1,(200,0,200),1,cv2.LINE_AA)
+            
+            orig = cv2.resize( orig , None, fx=0.5, fy=0.5)
+            img = cv2.cvtColor(orig, cv2.COLOR_BGR2GRAY)
+	    img=cv2.medianBlur(img,5)
+            img=cv2.adaptiveThreshold(img,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
+                    cv2.THRESH_BINARY,9,5)#
+            self.steer(img)
+	    self.pub.publish(self.msgValue)
+	    cv2.imshow('image', img)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                return
+          
+        except Exception as err:
+            print (err)
+    def controlingbot(self,data):
+        if data.argmax(-1)==0:#+20
+            self.state=self.class_names[0]
+            self.msgValue.L=5
+            self.msgValue.R=50
+            # 1-- 0.6
+        elif data.argmax(-1)==1:
+            self.state=self.class_names[1]
+            self.msgValue.L=40
+            self.msgValue.R=5
+        elif data.argmax(-1)==2:
+            self.state=self.class_names[2]
+            self.msgValue.L=50
+            self.msgValue.R=50
+        elif data.argmax(-1)==3:
+            self.state=self.class_names[3]
+            self.msgValue.L=-30
+            self.msgValue.R=-30
+
+
+    def steer(self,img):  
+        global graph
+        with graph.as_default():
+            height,width =img.shape#
+            img = img[int(height/2):height, :]
+            img = img.reshape(1, int(height/2) * width).astype(np.float32)
+            img=img/255.
+            img=img.reshape(120,320,1) 
+            self.controlingbot(model.predict(np.expand_dims(img, axis=0))[0])
+
+
+
+
+
+
+        
+if __name__ == '__main__':
+    
+    start=recognizing_img()
+    print ("sending command...")
+    rospy.Subscriber("/image/compressed", CompressedImage, start.img_bridge,queue_size = 3)
+    while not rospy.is_shutdown():
+        rospy.spin()
+
+
+
+
---
+++ collecting_data.py
@@ -0,0 +1,154 @@
+#!/usr/bin/env python 
+__author__='MED BKB'
+
+import pygame as pg
+import rospy
+import time
+import cv2
+import numpy as np
+import os
+from publish_tuto.msg import velo
+from sensor_msgs.msg import CompressedImage
+#sim Vrep from sensor_msgs.msg import Image--from cv_bridge import CvBridge
+#from sim_ws.msg import velocity
+#from std_msgs.msg import Float32
+#pygame
+
+
+class Coll_TrainingData(object):
+    def __init__(self,st_tick):
+        self.input_size = 120*320
+	#vrep 128*256
+        # create labels
+        self.k = np.zeros((4, 4), 'float')
+        for i in range(4):
+            self.k[i, i] = 1
+        #paygame
+        self.saved_frame = 0
+        self.total_frame = 0
+        self.frame=0
+        self.st_tick=st_tick
+        #ros
+        rospy.init_node('velocity_command')
+        self.pub=rospy.Publisher('/topic_value',velo,queue_size=1)
+        self.msgValue=velo()
+        self.X = np.empty((0, self.input_size))
+        self.y = np.empty((0, 4))
+
+        
+    def img_bridge(self,msg):
+        try:
+            
+            np_arr = np.fromstring(msg.data, np.uint8)
+            image_np = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
+
+            ########=====sim Vrep=== # convert sensor_msgs/Image to OpenCV Image
+            # bridge = CvBridge()
+            # orig = bridge.imgmsg_to_cv2(msg, "bgr8")
+            orig = cv2.flip(image_np,-1)
+            
+            orig = cv2.resize( orig , None, fx=0.5, fy=0.5)
+        #print orig.shape
+            img = cv2.cvtColor(orig, cv2.COLOR_BGR2GRAY)
+            cv2.imshow('image', img)
+            self.collecting(img)
+            self.pub.publish(self.msgValue)
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                return
+          
+        except Exception as err:
+            print err
+
+    def collecting(self,img):  
+        
+        
+        height,width =img.shape
+        roi = img[int(height/2):height, :]
+        temp_array = roi.reshape(1, int(height/2) * width).astype(np.float32)
+            
+        self.frame += 1
+        self.total_frame += 1
+
+        for event in pg.event.get():
+                key_input = pg.key.get_pressed()
+                if event.type==pg.QUIT:
+                     return
+                if key_input[pg.K_UP]:
+                    print "FORWARD at -1 [0010] "
+                    #100
+                    #self.publ.publish(-1)
+                    #self.pubr.publish(-1)
+                    self.msgValue.L=20
+                    self.msgValue.R=25
+                    self.X = np.vstack((self.X, temp_array))
+                    self.y = np.vstack((self.y, self.k[2]))
+                    self.saved_frame += 1
+                elif key_input[pg.K_DOWN]:
+                    print "BACKWARD at 1 [0001]"
+                    #-100
+                    #self.publ.publish(1)
+                    #self.pubr.publish(1)
+                    self.msgValue.L=-20
+                    self.msgValue.R=-20
+                    self.X = np.vstack((self.X, temp_array))
+                    self.y = np.vstack((self.y, self.k[3]))
+                    self.saved_frame += 1
+                elif  key_input[pg.K_RIGHT]:
+                    print "RIGHT at -0.7 -1 [1000]"
+                    #100-50
+                    #self.publ.publish(-0.6)
+                    #self.pubr.publish(-1)
+                    self.msgValue.R=20
+                    self.msgValue.L=5
+                    self.X = np.vstack((self.X, temp_array))
+                    self.y = np.vstack((self.y, self.k[0]))
+                    self.saved_frame += 1
+                elif  key_input[pg.K_LEFT]:
+                    print "LEFT at -1 -0.7 [0100]"
+                    #self.publ.publish(-1)
+                    #self.pubr.publish(-0.6)
+                    self.msgValue.R=5
+                    self.msgValue.L=20
+                    self.X = np.vstack((self.X, temp_array))
+                    self.y = np.vstack((self.y, self.k[1]))
+                    self.saved_frame += 1
+                elif  key_input[pg.K_RSHIFT] :
+                    print "STOP"
+                    #self.publ.publish(0)
+                    #self.pubr.publish(0)
+                    self.msgValue.L=0
+                    self.msgValue.R=0
+                    file_name = str(int(time.time()))
+                    directory = "data_collected"
+                    if not os.path.exists(directory):
+                        os.makedirs(directory)
+                    try:
+                        np.savez(directory + '/' + file_name + '.npz', train=self.X, train_labels=self.y)
+                    except IOError as e:
+                        print(e)
+                    end = cv2.getTickCount()
+            # calculate streaming duration
+                    print("Streaming duration: , %.2fs" % ((end - self.st_tick) / cv2.getTickFrequency()))
+
+                    print(self.X.shape)
+                    print(self.y.shape)
+                    print("Total frame: ", self.total_frame)
+                    print("Saved frame: ", self.saved_frame)
+                    print("Dropped frame: ", self.total_frame - self.saved_frame)
+	       
+       
+
+if __name__ == '__main__':
+    pg.init()
+    scr=pg.display.set_mode((480,240))
+    st_tick=cv2.getTickCount()
+    start=Coll_TrainingData(st_tick)
+    print "sending command..."
+#simulation v-rep /image,Image
+    rospy.Subscriber("/image/compressed", CompressedImage, start.img_bridge,queue_size = 3)
+    print("Start collecting images...")
+    pg.display.update()
+    rospy.spin()
+   
+
+
